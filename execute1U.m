66fbb9998e958ea32df05a6508d5729e9351ad60
Turux <35221455+Turux@users.noreply.github.com>
2019-03-29 09:25:59 +0000

    Plotting fixed

--- execute1U.m
+++ execute1U.m
@@ -5,16 +5,15 @@ close all
 IMU = importfile(fullfile(PathName,FileName));
 
 IMU=prepare(IMU,100);
-Right=prepare(Right,100);
 
 figure
 ax1=subplot(3,1,1);
 plot(IMU.ms,IMU.ax)
 title('Ax')
-ax2=subplot(3,1,3);
+ax2=subplot(3,1,2);
 plot(IMU.ms,IMU.ay)
 title('Left Ay')
-ax3=subplot(3,2,5);
+ax3=subplot(3,1,3);
 plot(IMU.ms,IMU.az)
 title('Left Az')
 linkaxes([ax1,ax2,ax3],'x')
@@ -24,13 +23,13 @@ plot(IMU.ms,IMU.a)
 title('mod(A)')
 
 figure
-ax1=subplot(3,2,1);
+ax1=subplot(3,1,1);
 plot(IMU.ms,IMU.gx)
 title('Gx')
-ax2=subplot(3,2,3);
+ax2=subplot(3,1,2);
 plot(IMU.ms,IMU.gy)
 title('Gy')
-ax3=subplot(3,2,5);
+ax3=subplot(3,2,3);
 plot(IMU.ms,IMU.gz)
 title('Gz')
 linkaxes([ax1,ax2,ax3],'x')
